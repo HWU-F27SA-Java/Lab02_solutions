98610b021d871b1424bdc846ad29f398d82902d1
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:44:40 +0100

    Create Exercise1.java

---
+++ src/Exercise1.java
@@ -0,0 +1,19 @@
+/**
+ * Week 2, Exercise 1.
+ * @author Michael Lones
+ */
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// Time variables
+		int daysInYear = 365;
+		int hoursInDay = 24;
+		int minutesInHour = 60;
+		
+		// Print them out
+		System.out.println(daysInYear);
+		System.out.println(hoursInDay);
+		System.out.println(minutesInHour);
+	}
+
+}
